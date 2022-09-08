7bf5620e3352c88884c52fcc487ca4d0f9602080
Kalvin Weng <>
2022-09-08 03:16:09 +0000

    Updated Exercise for chapter 2

--- ch02/exercises/main.py
+++ ch02/exercises/main.py
@@ -1,28 +1,53 @@
 import turtle
+wn = turtle.Screen()          
+# Set up the window and its attributes
+wn.bgcolor("pink")
+wn.screensize(1000, 800)
 
 
-t = turtle()
+tess = turtle.Turtle() 
+tess.shape("turtle")
+# create tess and draw a square
+tess.color("purple")
+tess.pensize(5)
+for i in range(4):
+    tess.left(90)
+    tess.forward(50)
 
-t.foward(80)
+tess.up()
+tess.goto(50,50)
+# create uber to draw another square
+turtle.down()
+uber = turtle.Turtle()  
+uber.up()
+uber.goto(50,50)
+uber.down()
+uber.color("red")
+for i in range(4):
+    uber.left(90)
+    uber.forward(50)
 
 
 
-# turtle.forward(15)
-# turtle.right(25)
-# t.Screen
-# for c in ['red', 'green', 'blue', 'yellow']:
-#     t.color(c)
-#     t.forward(75)
-#     t.left(90)
+
+# turtle draw any shape
+vertex = turtle.Turtle()
+vertex.up()
+vertex.goto(150,-50)
+vertex.down()
+userColor = input("What color?")
+
+vertex.color(userColor)
+vertex.pensize(3)
+vertex.shape("turtle")
+numberOfSides = int(input("How sides do you want your shape to have?"))
+
+print (numberOfSides)
+Angle = 360 // numberOfSides
+
+for i in range(numberOfSides):
+    vertex.left(Angle)
+    vertex.forward(50)
 
 
-
-# from turtle import turtle
-# # don't need to type turtle anymore but if another thing is turtle, will conflict with the code
-
-# my_turtle.color("purple")
-# length = 50
-# my_turtle.foward(length)
-
-# window = turtle.screen()
-# window.bycolor(orange)
+wn.exitonclick()
