93d8c6b8f976a277ebf34cc79424c8eae0dc8fc3
Kalvin Weng <>
2022-09-08 04:09:06 +0000

    Ch02

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,16 +1,26 @@
+import random
+
 
 #Part A
 weeks = 16
 classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
+print("Cost per week:", cost_per_week, type(cost_per_week))
 
 
 classes_per_week = 3
-print("Classes per week " + classes_per_week,type(classes_per_week))
+print("Classes per week ", classes_per_week ,type(classes_per_week))
 
-cost_per_class = cost_per_week // classes_per_week
-print("cost_per_class")
+cost_per_class = int(cost_per_week // classes_per_week)
+print("Everyday, you're paying $"+ str(cost_per_class) +" for each of your classes! Thank you for your service!",type(cost_per_class))
 
 #Part B
+random.randrange(1,10)
+
+myList = ["phone", "4" , 0.5, "CS110", 0, "food"]
+print(myList)
+numberOfOutput = int(input("How many random output do you want?"))
+for i in range(numberOfOutput):
+  myRandom =  random.choice(myList)
+  print(myRandom, type(myRandom))
